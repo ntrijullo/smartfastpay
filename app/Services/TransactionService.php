6410f3f65637be563e6e39af92a4def2033077d5
Enzo Trujillo <enzot.13@gmail.com>
2024-08-08 12:24:31 -0400

    add class to pay transaction

---
+++ app/Services/TransactionService.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Services;
+
+use App\Models\Payment;
+use Exception;
+
+class TransactionService
+{
+    public $total_amount;
+
+    public function pay(Payment $payment)
+    {
+        $this->total_amount = $this->totalAmount($payment); 
+        try{
+            //simula tiempo respuesta pago
+            sleep(2);
+            $rand = mt_rand(1,10);
+            return $rand <=7;
+        }catch(Exception $e){
+            return false;
+        }
+
+    }
+
+    private function totalAmount(Payment $payment)
+    {
+        $total = 0;
+
+        switch ($payment['payment_method']) {
+            case 'pix':
+                $discountPercentage = 1.5;
+                $total = $payment['value'] * (1 - $discountPercentage / 100);
+                break;
+            case 'boleto':
+                $discountPercentage = 2;
+                $total = $payment['value'] * (1 - $discountPercentage / 100);
+                break;
+            case 'transferencia_bancaria':
+                $discountPercentage = 4;
+                $total = $payment['value'] * (1 - $discountPercentage / 100);
+                break;
+        }
+        return $total;
+    }
+}
